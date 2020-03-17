e85244b7920454425404e1e55026dea1afe7cf57
spasia3run <spasia3run@gmail.com>
2020-03-17 17:51:04 +0200

    add new project top-sports

---
+++ top-sports/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+  // $('input[placeholder], textarea[placeholder]').placeholder();
+  // $(".mask-phone").mask("+7 (999) 999-99-99");
+
+  // add-open-class
+  $('.burger-menu').click(function(){
+   if($(this).parent().is('.menu-burger--opened')){
+     $(this).parent().removeClass('menu-burger--opened');
+     $('body').removeClass('menu-open-wrapper-page');
+   }else{
+     $(this).parent().addClass('menu-burger--opened');
+     $('body').addClass('menu-open-wrapper-page');
+   }
+  });
+
+  // add-open-class
+  $('.menu-item__link--dd-open').click(function(){
+   if($(this).parent().is('.menu-item--dd-opened')){
+    $(this).parent().removeClass('menu-item--dd-opened');
+   }else{
+    $(this).parent().addClass('menu-item--dd-opened');
+   }
+  });
+
+});
